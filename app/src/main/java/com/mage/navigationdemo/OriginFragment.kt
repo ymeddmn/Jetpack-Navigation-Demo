52715cb09420ced8d82c2dab165a37a6f97c6848
mage <1275054668@qq.com>
2021-06-05 10:58:45 +0800

    first commit

---
+++ app/src/main/java/com/mage/navigationdemo/OriginFragment.kt
@@ -0,0 +1,57 @@
+package com.mage.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OriginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OriginFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val content = inflater.inflate(R.layout.fragment_origin, container, false)
+        val btn = content.findViewById<Button>(R.id.btn_todistination)
+        btn.setOnClickListener {
+            val findNavController = findNavController()
+            findNavController.navigate(R.id.destiationFragment)
+        }
+
+        return content
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            OriginFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
