6a0f404c6ac8319d8d9989b18401fffea78eb8ab
mage <1275054668@qq.com>
2021-06-09 21:36:41 +0800

    提交popup的两个方法

--- app/src/main/java/com/mage/navigationdemo/PopUpFragment.kt
+++ app/src/main/java/com/mage/navigationdemo/PopUpFragment.kt
@@ -5,18 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [AFragment.newInstance] factory method to
+ * Use the [PopUpFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AFragment : Fragment() {
+class PopUpFragment : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -26,28 +27,29 @@ class AFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-        println("AFragment被创建")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val content = inflater.inflate(R.layout.fragment_a, container, false)
-        content.findViewById<Button>(R.id.btn_toBFragment).setOnClickListener {
-            findNavController().navigate(R.id.action_AFragment_to_BFragment)
-        }
-
-        content.findViewById<Button>(R.id.btn_toPopFragment).setOnClickListener {
-
-        }
-        return content
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pop_up, container, false)
     }
 
     companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment PopUpFragment.
+         */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            AFragment().apply {
+            PopUpFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
